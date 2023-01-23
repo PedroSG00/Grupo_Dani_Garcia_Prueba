62a93ac44fa6eaeaf22a4f2f651b7b4f0eae79f3
PedroSG00 <suarezgallardopedro@gmail.com>
2023-01-23 10:41:30 +0100

    Controler done

--- server/routes/cat-fact-and-gif.routes.js
+++ server/routes/cat-fact-and-gif.routes.js
@@ -1,30 +1,7 @@
 const router = require("express").Router();
-const catFactApiService = require('../services/cats-facts-api-service')
-const giphyApiService = require('../services/giphy-api-service')
+const { getFactAndGif } = require('../controller/fact-and-image.controller')
 
-router.get("/", async (req, res, next) => {
-
-    try {
-
-        const catFact = await catFactApiService.getCatFact()
-        const { data } = catFact
-        const cuttedFact = data.fact.split(' ').slice(0, 3).join(" ")
-
-        const gif = await giphyApiService.getGif(cuttedFact)
-        const randomIndexSearch = Math.floor(Math.random() * (gif.data.data.length - 1 - 0 + 1) + 1)
-
-        const giphyImage = gif.data.data[randomIndexSearch].images.original.webp
-        const imageAndFact = { fact, giphyImage }
-
-        res.status(200).json(imageAndFact)
-
-    } catch (err) {
-
-        next(err)
-
-    }
-
-});
+router.get("/", getFactAndGif)
 
 
 module.exports = router;
